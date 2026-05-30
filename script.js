0e3b1c4e8cf59502ca4f679fd4f6b21282cdf589
lucasminato146-ops <lucasminato146@gmail.com>
2026-05-30 15:45:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function mostrarDescricao() {
-    const descricao = document.getElementById("descricao");
 
-    if(descricao.style.display === "block"){
-        descricao.style.display = "none";
-    }else{
-        descricao.style.display = "block";
-    }
+    const descricao = document.getElementById("descricao");
+    const botao = document.getElementById("infoBtn");
+
+    descricao.style.display = "block";
+
+    botao.style.opacity = "0";
+
+    setTimeout(() => {
+        botao.style.display = "none";
+    }, 300);
+
 }
